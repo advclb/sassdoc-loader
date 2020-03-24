4eff9f9c28ec02696a4ce233fd7a724a590bf99a
Guo Yunhe <i@guoyunhe.me>
2020-03-24 21:26:24 +0200

    Initial commit

---
+++ examples/advanced/README.md
@@ -0,0 +1,5 @@
+# Advanced Example
+
+```
+npm start
+```
---
+++ tests/default.spec.js
@@ -0,0 +1,7 @@
+const foobar = require("../lib/index");
+
+describe("foobar", () => {
+  it("should foobar", () => {
+    expect(foobar()).toBeNull();
+  });
+});
